ebad86b6787103fb94db6b2be1b7d93f309f6908
Jakub Kołuda <jko01@outlook.com>
2022-10-04 11:38:30 +0200

    Add header content, styles and script.

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const header = document.querySelector(".header")
+const headerButton = document.querySelector(".button-header");
+
+const allText = document.querySelector(".header__animated-name");
+const topText = document.querySelector(".header__animated-name-container");
+const bottomText = document.querySelector(".header__animated-info-span");
+
+
+
+const hideHeader = () => {    
+    headerButton.classList.add("button-header--animOut");
+    bottomText.classList.add("header__animated-info-span--animOut");
+    topText.classList.add("header__animated-name-container--animOut");
+    allText.classList.add("header__animated-name--animOut");
+
+    setTimeout(() => {
+        header.style.top = "100%";
+    }, 1000);
+}
+
+headerButton.addEventListener("click", hideHeader);
